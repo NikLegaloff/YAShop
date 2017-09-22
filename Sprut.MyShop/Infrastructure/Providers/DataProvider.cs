c6a85fdf7de99b89457f2cba06efea95e62500e9
Nik <niklegaloff@gmail.com>
2017-09-22 16:18:10 +0300

    add items to IdentityMap after bulk select

--- Sprut.MyShop/Infrastructure/Providers/DataProvider.cs
+++ Sprut.MyShop/Infrastructure/Providers/DataProvider.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using Sprut.MyShop.Domain;
 
@@ -15,22 +16,33 @@ namespace Sprut.MyShop.Infrastructure.Providers
 
         public T Find(Guid id)
         {
-            return _executor.Find(id);
+            var subj = (T)IdentityMap[id.ToString()];
+            if (subj == null)
+            {
+                subj = _executor.Find(id);
+                Registry.Current.CommonInfrastructureProvider.IdentityMap.Add(id.ToString(),subj);
+            }
+            return subj;
         }
 
         public void Save(T subj)
         {
+            if (!IdentityMap.Contains(subj.Id)) IdentityMap.Add(subj.Id, subj);
             _executor.Save(subj);
         }
 
         public void Delete(T subj)
         {
+            if (IdentityMap.Contains(subj.Id)) IdentityMap.Remove(subj.Id);
             _executor.Delete(subj);
         }
 
         public List<T> Select(string query=null, dynamic param=null)
         {
-            return _executor.Select(query, param);
+            var result = _executor.Select(query, param);
+            if (result!=null) foreach (var subj in result) if (!IdentityMap.Contains(subj.Id)) IdentityMap.Add(subj.Id, subj);
+            return result;
         }
+        private static IDictionary IdentityMap => Registry.Current.CommonInfrastructureProvider.IdentityMap;
     }
 }
