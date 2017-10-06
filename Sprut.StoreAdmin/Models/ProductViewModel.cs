fe9e78a0993d4ecfab41cf0903ffed259641dd2e
Sprut <sprutmail@gmail.com>
2017-10-06 14:39:39 +0300

    ProductView start

--- Sprut.StoreAdmin/Models/ProductViewModel.cs
+++ Sprut.StoreAdmin/Models/ProductViewModel.cs
@@ -2,18 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using Sprut.MyShop.Domain;
 
 namespace Sprut.StoreAdmin.Models
 {
     public class ProductViewModel
     {
         public int CurrentPage;
-        public string Order;
 
+        //filter
+        public string Order;
+        public string CategoryId;
+        public string Name;
+        public string MinPrice;
+        public string MaxPrice;
+
+        
         public ProductViewModel()
         {
             CurrentPage = 1;
-            Order = "SKU";
         }
     }
 
