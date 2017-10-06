a6f4ef7a1897f6b2d2cf9c129758b3c56da7c100
Nik <niklegaloff@gmail.com>
2017-10-06 15:20:25 +0300

    Credentials for DB

--- Sprut.MyShop/Domain/EFContext.cs
+++ Sprut.MyShop/Domain/EFContext.cs
@@ -9,7 +9,18 @@ namespace Sprut.MyShop.Domain
 {
     public class EfContext : DbContext
     {
-        public EfContext() : base("Data Source=SPR\\SQLEXPRESS;Initial Catalog=YAShopDBv1;Integrated Security=True") { }
+        public EfContext() : base(GetConnectionString) { }
+
+        private static string GetConnectionString
+        {
+            get
+            {
+                
+                if (Environment.MachineName == "KOT") return "Data Source=.;Initial Catalog=YAShopDBv1;Persist Security Info=True;User ID=sa;Password=Password1";
+                return "Data Source==SPR\\SQLEXPRESS;Initial Catalog=YAShopDBv1;Integrated Security=True";
+            }
+        }
+
         //public EfContext() : base("d:\\Temp\\YAShopDBv1.mdf") { }
 
         public DbSet<Product> Product { get; set; }
