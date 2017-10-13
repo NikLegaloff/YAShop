03ee012e7b93ba40b211fe0965e5a68d02458ee5
Sprut <sprutmail@gmail.com>
2017-10-13 16:18:06 +0300

    Category start

---
+++ Sprut.StoreAdmin/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sprut.MyShop.Domain;
+using Sprut.StoreAdmin.Models;
+using Sprut.MyShop.Infrastructure;
+
+namespace Sprut.StoreAdmin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private CategoryViewModels _cvModel=new CategoryViewModels();
+        [HttpGet]
+        public ActionResult Index()
+        {
+          
+            return View(_cvModel);
+        }
+
+        [HttpPost]
+        public ActionResult Index(CategoryViewModels model)
+        {
+            model.Category.ParentId = Guid.Parse(model.CategorySelectList.DataValueField);
+            Registry.Current.Categories.Save(model.Category);
+     
+            return View();
+        }
+
+        public ActionResult Edit(Guid id)
+        {
+            _cvModel.Category = Registry.Current.Categories.Find(id);
+            _cvModel.CategorySelectList = new SelectList(Registry.Current.Categories.GetPlanarTree(), "Id", "FullName",_cvModel.Category.ParentId);
+            return Index();
+        }
+    }
+}
---
+++ Sprut.StoreAdmin/Models/CategoryViewModels.cs
@@ -0,0 +1,25 @@
+﻿using Sprut.MyShop.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sprut.MyShop.Domain;
+
+namespace Sprut.StoreAdmin.Models
+{
+    public class CategoryViewModels
+    {
+        public List<CategoryTreeItem> CategoryTreeItems;
+        public SelectList CategorySelectList;
+        public Category Category { get; set; }
+
+        public CategoryViewModels()
+        {
+            CategoryTreeItems = Registry.Current.Categories.GetPlanarTree();
+
+            CategorySelectList = new SelectList(Registry.Current.Categories.GetPlanarTree(), "Id", "FullName");
+        }
+    }
+    
+}
