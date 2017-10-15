5e52c2573ff83b1f4d26b5a27584bc89e06e0123
sprut-tir <sprutmail@gmail.com>
2017-10-15 21:57:15 +0300

    Category done

--- Sprut.StoreAdmin/Controllers/CategoryController.cs
+++ Sprut.StoreAdmin/Controllers/CategoryController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
 using Sprut.MyShop.Domain;
 using Sprut.StoreAdmin.Models;
 using Sprut.MyShop.Infrastructure;
@@ -12,27 +13,53 @@ namespace Sprut.StoreAdmin.Controllers
     public class CategoryController : Controller
     {
         private CategoryViewModels _cvModel=new CategoryViewModels();
+
+        public object MessageBox { get; private set; }
+
         [HttpGet]
-        public ActionResult Index()
+        public ActionResult Index(Guid? id)
         {
-          
+            if (id!=null)
+            {
+                _cvModel.Category = Registry.Current.Categories.Find((Guid)id);
+                _cvModel.CategorySelectList = new SelectList(Registry.Current.Categories.GetPlanarTree(), "Id", "FullName", _cvModel.Category.ParentId);
+                return View(_cvModel);
+            }
             return View(_cvModel);
         }
 
         [HttpPost]
         public ActionResult Index(CategoryViewModels model)
         {
-            model.Category.ParentId = Guid.Parse(model.CategorySelectList.DataValueField);
-            Registry.Current.Categories.Save(model.Category);
-     
-            return View();
+            Category category;
+            if (Registry.Current.Categories.Find(model.Category.Id) != null)
+            {
+                category = Registry.Current.Categories.Find(model.Category.Id);
+                category.Name = model.Category.Name;
+                category.ParentId = model.Category.ParentId;
+            }
+            else
+            {
+                category = new Category();
+                category.Name = model.Category.Name;
+                category.ParentId = model.Category.ParentId;
+            }
+            Registry.Current.Categories.Save(category);
+            return Redirect("/Category/Index");
         }
 
-        public ActionResult Edit(Guid id)
+        public ActionResult Delete(Guid id)
         {
-            _cvModel.Category = Registry.Current.Categories.Find(id);
-            _cvModel.CategorySelectList = new SelectList(Registry.Current.Categories.GetPlanarTree(), "Id", "FullName",_cvModel.Category.ParentId);
-            return Index();
+            if (Registry.Current.Categories.Select("WHERE ParentId=@parentId", new {parentId = id}).Count > 0)
+            {
+                Response.Write("<script>window.alert('ku-ku');</script>");
+                return Redirect("/Category/Index");
+            }
+            Category category = Registry.Current.Categories.Find(id);
+            Registry.Current.Categories.Delete(category);
+            return Redirect("/Category/Index");
+
         }
+
     }
 }
