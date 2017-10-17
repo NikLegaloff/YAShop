1d9e8070d324f1c94a4cad2e2e2c82e35fa549af
Sprut <sprutmail@gmail.com>
2017-10-17 16:30:42 +0300

    micro fix

--- Sprut.StoreAdmin/Controllers/ProductController.cs
+++ Sprut.StoreAdmin/Controllers/ProductController.cs
@@ -132,32 +132,23 @@ namespace Sprut.StoreAdmin.Controllers
 
         public ActionResult GroupAction()
         {
-            var action = Request.Form["action"];
-            //var selectedproducts = Request.Form["selectedproducts"];
-            //selectedproducts = Regex.Replace(selectedproducts, ",", "','");
-            //param.selectedproducts = $"'" + selectedproducts + "'";
-
             //I'm very cool )))
             var selectedproducts = $"'" + Regex.Replace(Request.Form["selectedproducts"], ",", "','") + "'";
             var listProduct = Registry.Current.Products.Select($"where Id in (" + selectedproducts + ")");
 
-            switch (action)
+            switch (Request.Form["action"])
             {
                 case "delete":
-                    //var listProduct = Registry.Current.Products.Select($"where Id in ("+selectedproducts+")");
                     foreach (var p in listProduct)
                     {
                         Registry.Current.Products.Delete(Registry.Current.Products.Find(p.Id));
                     }
                     break;
                 case "remove":
-                    //var listProduct = Registry.Current.Products.Select($"where Id in (" + selectedproducts + ")");
                     foreach (var p in listProduct)
                     {
                         var product = Registry.Current.Products.Find(p.Id);
-                        var temp = Request.Form["moveCategory"];
-                        var newCat = Guid.Parse(Request.Form["moveCategory"]);
-                        product.CategoryId = newCat;
+                        product.CategoryId = Guid.Parse(Request.Form["moveCategory"]);
                         Registry.Current.Products.Save(product);
                     }
                     break;
