41e6f070eaaba3bf27fcbb7fe396558a26f78332
Sprut <sprutmail@gmail.com>
2017-10-31 09:50:29 +0200

    Add Db class and using

---
+++ ImageStore/Db.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageStore
+{
+    public class Db
+    {
+        //string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        private const string ConnectionString = "Data Source = (LocalDB)\\MSSQLLocalDB;AttachDbFilename='E:\\Visual Studio 2017\\Projects\\YAShop\\ImageStore\\App_Data\\ImageStore.mdf';Integrated Security = True";
+
+        public static SqlConnection Open()
+        {
+            var connection = new SqlConnection(ConnectionString);
+            connection.Open();
+            return connection;
+        }
+    }
+}
--- ImageStore/ImageStoreRepository.cs
+++ ImageStore/ImageStoreRepository.cs
@@ -14,10 +14,6 @@ namespace ImageStore
 {
     public class ImageStoreRepository
     {
-        //string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
-        private string connectionString =
-                "Data Source = (LocalDB)\\MSSQLLocalDB;AttachDbFilename='E:\\Visual Studio 2017\\Projects\\YAShop\\ImageStore\\App_Data\\ImageStore.mdf';Integrated Security = True"
-            ;
         public void AddFolder(string name, string path)
         {
             var folder = new Folder
@@ -25,18 +21,22 @@ namespace ImageStore
                 Name = name,
                 ParentId = PathToParentId(path)
             };
-            IDbConnection dbConnection=new SqlConnection(connectionString);
-            var sqlQuery = "INSERT INTO Folders (Id, ParentId, Name) VALUES (@Id, @ParentID, @Name)";
-            dbConnection.Query<Folder>(sqlQuery, folder);
+            using (var db = Db.Open())
+            {
+                var sqlQuery = "INSERT INTO Folders (Id, ParentId, Name) VALUES (@Id, @ParentID, @Name)";
+                db.Query<Folder>(sqlQuery, folder);
+            }
         }
 
         public Folder GetFolder(string name, Guid? parentId)
         {
-            string sqlQuery= "SELECT * FROM Folders WHERE Name=@name and ParentId";
-            if (parentId == null) sqlQuery += " is null";
-            else sqlQuery += "=@parentId";
-            IDbConnection dbConnection = new SqlConnection(connectionString);
-            return dbConnection.Query<Folder>(sqlQuery, new {name, parentId}).FirstOrDefault();
+            using (var db = Db.Open())
+            {
+                string sqlQuery = "SELECT * FROM Folders WHERE Name=@name and ParentId";
+                if (parentId == null) sqlQuery += " is null";
+                else sqlQuery += "=@parentId";
+                return db.Query<Folder>(sqlQuery, new {name, parentId}).FirstOrDefault();
+            }
         }
 
         public Guid? PathToParentId(string path)
@@ -68,9 +68,11 @@ namespace ImageStore
                Name = fileName,
                Folder = (Guid) PathToParentId(folder)
             };
-            IDbConnection dbConnection = new SqlConnection(connectionString);
-            var sqlQuery = "INSERT INTO Images (Id, Folder, Name) VALUES (@Id, @Folder, @Name)";
-            dbConnection.Query<Image>(sqlQuery, image);
+            using (var db = Db.Open())
+            {
+                var sqlQuery = "INSERT INTO Images (Id, Folder, Name) VALUES (@Id, @Folder, @Name)";
+                db.Query<Image>(sqlQuery, image);
+            }
         }
 
     }
