1ad91ef4f34df797f5afd9c58e5702d75d731a21
Nik <niklegaloff@gmail.com>
2024-03-14 19:48:47 +0200

    catalog fixes publish added +Сrib

---
+++ src/YAShop.Common/Сrib.cs
@@ -0,0 +1,41 @@
+﻿namespace YAShop.Common;
+
+public class Dog
+{
+    public Dog(){}
+    public Dog(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public string Name { get; set; }
+    public int Age{ get; set; }
+}
+public class Сrib
+{
+    public void Numbers()
+    {
+        var sum = "1,2,3,4,5,6,7,7,8,8,8".Split(',').Distinct().Select(s=>int.Parse(s)).Where(n=>n>2).Sum(n=>n);
+    }
+
+    public void Dogs()
+    {
+        var dogs = "Nik,2;Josef,3".Split(';').Select(s => new Dog { Name = s.Split(',')[0], Age = int.Parse(s.Split(',')[0])}).ToList();
+        dogs.Add(new Dog{Name = "Bobic",Age = 12});
+        dogs.Insert(0,new Dog("Tuzic",4));
+        var first = dogs.First();
+        var first2 = dogs.Take(2).ToArray();
+        var last3 = dogs.TakeLast(3).ToArray();
+        var allExceptFirst2 = dogs.Skip(2);
+
+        dogs.Sort((d1, d2) => d1.Name.CompareTo(d2.Name));
+        dogs.Reverse();
+
+        dogs.RemoveAll(d => d.Age > 10);
+
+        var nik = dogs.Find(d => d.Name == "Nik");
+        var oldestName = (from dog in dogs where dog.Age == dogs.Select(d=>d.Age).Max() select dog.Name).First();
+        var ageSum = dogs.Sum(d => d.Age);
+    }
+}
--- src/YAShop.Web.Storefront/Controllers/CatalogController.cs
+++ src/YAShop.Web.Storefront/Controllers/CatalogController.cs
@@ -10,14 +10,11 @@ public class CatalogController : YASControllerBase
     {
         return View(new ProductViewModel{Product = Registry.Current.Products.Find(id) ,SimilarProducts = SelectProductSummary(Registry.Current.Products.SelectAll().Skip(12).Take(4).ToArray()) });
     }
-    public IActionResult Index(string? query=null, Guid? categotyId=null)
+    public IActionResult Index(string? query=null, Guid? categoryId=null)
     {
         var all = Registry.Current.Products.SelectAll().ToArray();
-        if (!string.IsNullOrEmpty(query))        
-             all = all.Where(p => p.Title.Contains(query, StringComparison.OrdinalIgnoreCase)).ToArray();
-
-        if (categotyId != null)
-            all = all.Where(p => p.StoreCategoryId == categotyId.Value).ToArray();
+        if (!string.IsNullOrEmpty(query)) all = all.Where(p => p.Title.Contains(query, StringComparison.OrdinalIgnoreCase)).ToArray();
+        if (categoryId != null) all = all.Where(p => p.StoreCategoryId == categoryId.Value).ToArray();
         
         return View(new CatalogModel() { Products = SelectProductSummary(all) });
     }
