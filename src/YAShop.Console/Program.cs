1dacd27cb62f6659dd05d629c5f50fe9e26d013a
Nik <niklegaloff@gmail.com>
2024-05-31 10:44:37 +0300

    213

--- src/YAShop.Console/Program.cs
+++ src/YAShop.Console/Program.cs
@@ -1,14 +1,7 @@
-﻿using System.IO;
-using System.Reflection;
-using Csv;
-using Newtonsoft.Json;
+﻿using System.Reflection;
 using YAShop.Common;
-using YAShop.Common.Domain;
-using YAShop.Common.Service.Cart;
 using YAShop.Console;
-using static System.Net.Mime.MediaTypeNames;
 
 var paths = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location).Split('\\');
 var path = string.Join("\\", paths.Take(paths.Length - 4)) + "\\YAShop.Web.Storefront\\wwwroot\\data\\";
 Registry.Init(new StaticCommonInfrProvider(), path);
-
